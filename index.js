3af79ed7d968c2346aa49d01bfadfc5a2336c5a9
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-10-09 00:41:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const socketIo  = require('socket.io');
+
+
+const server = http.createServer(app);
+
+const io = socketIo(server)
+
+// app.use(express.static('public'));
+
+app.get('/' , (req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+io.on('connection',(socket)=>{
+   socket.on('user-message',(messy)=>{
+    io.emit("message",messy)
+   })
+})
+
+server.listen(3000, () => console.log('Server running on port 3000'));
